1cc9d03bec39cb4eb54b881036819667ffaed260
Mateusz Skoczek <mateusz5432160@gmail.com>
2026-01-18 18:17:41 +0100

    EventQueue.Setup uses universal parameters format

--- TimetableDesigner.Backend.Events/EventQueue.cs
+++ TimetableDesigner.Backend.Events/EventQueue.cs
@@ -4,8 +4,14 @@ namespace TimetableDesigner.Backend.Events;
 
 public abstract class EventQueue<TSelf> where TSelf : EventQueue<TSelf>
 {
-    public abstract void Setup<TBuilder>(IServiceCollection services, TBuilder configuration)
-        where TBuilder : EventQueueBuilder<TSelf>;
-    
-    public abstract void Setup(IServiceCollection services, string connectionString);
+    public void Setup<TBuilder>(IServiceCollection services, TBuilder configuration)
+        where TBuilder : EventQueueBuilder<TSelf> =>
+        Setup(services, configuration.GetConnectionParameters());
+
+    public void Setup(IServiceCollection services, string connectionString)
+        => Setup(services, connectionString.Split(';')
+                                                             .Select(x => x.Split('='))
+                                                             .ToDictionary(x => x.First(), x => x.Last()));
+
+    protected abstract void Setup(IServiceCollection services, IDictionary<string, string> connectionParameters);
 }
--- TimetableDesigner.Backend.Events/EventQueueBuilder.cs
+++ TimetableDesigner.Backend.Events/EventQueueBuilder.cs
@@ -1,3 +1,6 @@
 ﻿namespace TimetableDesigner.Backend.Events;
 
-public abstract class EventQueueBuilder<TQueue> where TQueue : EventQueue<TQueue>;
+public abstract class EventQueueBuilder<TQueue> where TQueue : EventQueue<TQueue>
+{
+    public abstract IDictionary<string, string> GetConnectionParameters();
+}
