5934f4c522069f975e62d9b8460dd3962617549b
Mateusz Skoczek <mateusz5432160@gmail.com>
2026-01-18 17:41:09 +0100

    EventQueue and EventQueueBuilder separated

--- TimetableDesigner.Backend.Events/EventQueue.cs
+++ TimetableDesigner.Backend.Events/EventQueue.cs
@@ -2,7 +2,10 @@
 
 namespace TimetableDesigner.Backend.Events;
 
-public abstract class EventQueue
+public abstract class EventQueue<TSelf> where TSelf : EventQueue<TSelf>
 {
-    public abstract void Setup(IServiceCollection services);
+    public abstract void Setup<TBuilder>(IServiceCollection services, TBuilder configuration)
+        where TBuilder : EventQueueBuilder<TSelf>;
+    
+    public abstract void Setup(IServiceCollection services, string connectionString);
 }
