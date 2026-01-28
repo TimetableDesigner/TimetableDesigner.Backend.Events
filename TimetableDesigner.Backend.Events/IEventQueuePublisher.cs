9182f320739451060194454be95a2b83701ce085
Mateusz Skoczek <mateusz5432160@gmail.com>
2026-01-28 20:10:50 +0100

    implementation added

--- TimetableDesigner.Backend.Events/IEventQueuePublisher.cs
+++ TimetableDesigner.Backend.Events/IEventQueuePublisher.cs
@@ -1,7 +1,10 @@
-﻿namespace TimetableDesigner.Backend.Events;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace TimetableDesigner.Backend.Events;
 
 public interface IEventQueuePublisher
 {
-    public Task PublishAsync<T>(T data) where T : class;
-    public Task PublishAsync<T>(string data, Type dataType) where T : class;
+    public sealed Task PublishAsync<T>(T data) where T : class => PublishAsync(JsonSerializer.Serialize(data), data.GetType());
+    public Task PublishAsync(string data, Type dataType);
 }
